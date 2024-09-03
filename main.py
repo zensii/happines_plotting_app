66537d6126ed9dc64ac7e3d90a060924f5591e95
svetl <svetlin.sii@gmail.com>
2024-09-03 14:55:26 +0300

    loaded the dataframe

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('happy.csv')
+cols = df.columns
 
 st.title('In Search for Happiness')
 
-x = st.selectbox('Select the data for the X-axis', options=('Option1', 'Option2'))
-y = st.selectbox('Select the data for the Y-axis', options=('Option1', 'Option2'))
+x = st.selectbox('Select the data for the X-axis', options=(col.upper() for col in cols))
+y = st.selectbox('Select the data for the Y-axis', options=(col.upper() for col in cols))
 
 st.subheader(f'{x} and {y}')
 
-x_data = [5,12,25,67,88]
-y_data = [12,14,16,17,43]
-figure = px.scatter(x=x_data, y=y_data , labels={'x': x, 'y': y})
+
+x_data = df[x.lower()]
+y_data = df[y.lower()]
+
+figure = px.scatter(x=x_data, y=y_data, labels={'x': x, 'y': y})
 
 st.plotly_chart(figure)
